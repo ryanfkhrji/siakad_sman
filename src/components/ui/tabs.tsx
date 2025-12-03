0f53bd4d2328154b002ec93c14ba7a27c40b30bf
ryanfkhrji <ryanfakhroji09@gmail.com>
2025-12-03 20:02:40 +0700

    feat: perbaikan ui tabs

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -24,7 +24,7 @@ function TabsList({
     <TabsPrimitive.List
       data-slot="tabs-list"
       className={cn(
-        "bg-muted text-muted-foreground inline-flex h-9 w-fit items-center justify-center rounded-lg p-[3px]",
+        "bg-primary/10 text-muted-foreground inline-flex h-fit w-fit items-center justify-center rounded-lg p-[3px]",
         className
       )}
       {...props}
